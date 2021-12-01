349c957d4f9d2a029c8f565a9bb2549b709bc5be
TheDominooos <55160640+TheDominooos@users.noreply.github.com>
2021-12-01 12:42:47 +0100

    Add files via upload

--- test_figury.py
+++ test_figury.py
@@ -1,12 +1,15 @@
 from obliczanie_figur import *
 
+
 def test_is_triangle_possible():
     result = Triangle(1, 1, 2, 3)
     assert result.is_triangle_possible(3, 4, 5)
 
+
 def test_is_rectangle_possible():
-    result = Rectangle(3,3)
+    result = Rectangle(3, 3)
     assert result.is_rectangle_possible(3, 3)
+
 
 def test_is_wheel_possible():
     result = Wheel(1)
